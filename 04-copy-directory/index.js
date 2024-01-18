28bcfbd52acd4d13ec46c3b3c9574268dc153b8e
Sacubu <solomka.roma@bk.ru>
2024-01-18 19:36:04 +0300

    docs: add JS code and NodeJS

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourceFolderPath = './04-copy-directory/files';
+const destinationFolderPath = './04-copy-directory/files-copy';
+
+async function copyDir(source, destination) {
+  try {
+    await fs.promises.access(destination, fs.constants.F_OK).catch(() => {
+      return fs.promises.mkdir(destination);
+    });
+
+    const files = await fs.promises.readdir(source, { withFileTypes: true });
+
+    for (const file of files) {
+      const sourcePath = path.join(source, file.name);
+      const destinationPath = path.join(destination, file.name);
+
+      if (file.isFile()) {
+        await fs.promises.copyFile(sourcePath, destinationPath);
+      } else if (file.isDirectory()) {
+        await copyDir(sourcePath, destinationPath);
+      }
+    }
+  } catch (error) {
+    console.error('Error copying directory:', error);
+  }
+}
+
+copyDir(sourceFolderPath, destinationFolderPath);
